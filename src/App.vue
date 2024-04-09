d23035f79749e256fa23907d7aa2566c7fb5452c
DavideAlovisio13 <alovisiod@gmail.com>
2024-04-09 12:39:57 +0200

    RELEASE new project

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <HeaderComponent />
+  <MainComponent />
+</template>
+
+<script>
+import HeaderComponent from './components/HeaderComponent.vue'; // importo il componente
+import MainComponent from './components/MainComponent.vue'; // importo il componente
+  export default {
+    name: 'App',// meglio che gli dò un nome a questo componente
+    components: {
+      HeaderComponent, // importo il componente dichiarandolo come oggetto 
+      MainComponent // importo il componente dichiarandolo come oggetto
+    },
+    data() {
+      return {
+        
+      }
+    }
+  }
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,16 @@
+<template>
+<main>
+    <img src="../assets/img/img1.jpg" alt="imm" />
+</main>
+</template>
+
+<script>
+export default {
+  name: "MainComponent",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped></style>
